9c000c206639880f6fd8ec2922207d1a0d23dcb9
Bagas Wastu <git@wastu.net>
2026-09-20 12:00:52 +0000

    dash: Set ConsumerIR HAL property during early boot

--- module/customize.sh
+++ module/customize.sh
@@ -90,6 +90,7 @@ set_perm "$MODPATH/common.sh" 0 0 0644
 set_perm "$MODPATH/rotation-state.sh" 0 0 0644
 set_perm "$APK" 0 0 0644
 set_perm "$MODPATH/system/vendor/etc/default_volume_tables.xml" 0 0 0644
+set_perm "$MODPATH/system.prop" 0 0 0644
 if [ -f "$MODPATH/vendor/bin/hw/android.hardware.ir-service.lineage" ]; then
   set_perm "$MODPATH/vendor/bin/hw/android.hardware.ir-service.lineage" 0 2000 0755
   chcon u:object_r:hal_ir_default_exec:s0 "$MODPATH/vendor/bin/hw/android.hardware.ir-service.lineage" 2>/dev/null || true
--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -9,5 +9,4 @@ resolve_resetprop || exit 1
 "$RESETPROP" -n ro.config.media_vol_steps 30
 "$RESETPROP" -n ro.vendor.audio.media.volume.steps 30
 "$RESETPROP" -n persist.vendor.audiohal.besloudness_state 1
-"$RESETPROP" -n ro.hardware.consumerir common
 delete_oem_unlock_prop || true
